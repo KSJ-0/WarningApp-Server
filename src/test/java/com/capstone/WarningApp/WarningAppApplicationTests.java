cb2e43ebc038dbca74c4cd4440023705330e8be2
KSJ-0 <kim.mail1124@gmail.com>
2024-09-03 22:04:00 +0900

    initial commit

---
+++ src/test/java/com/capstone/WarningApp/WarningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.WarningApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
